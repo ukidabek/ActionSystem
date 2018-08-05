e2157f47b7c3cc14e94cacd5178b3e64ff44151c
Ukidabek <ukidabek@gmail.com>
2018-08-05 18:55:16 +0200

    - countdown callback added.

--- General/WaitForSecondsAction.cs
+++ General/WaitForSecondsAction.cs
@@ -1,16 +1,28 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace ActionsSystem.General
 {
     public class WaitForSecondsAction : BaseAsyncAction
     {
         [SerializeField] private float _secondsToWait = 1f;
+        public CountdownCallback CountdownCallback = new CountdownCallback();
 
         public override IEnumerator Corutine(params object[] data)
         {
-            yield return new WaitForSeconds(_secondsToWait);
+            var _countDownCounter = _secondsToWait;
+
+            while (_countDownCounter > 0)
+            {
+                _countDownCounter -= Time.deltaTime;
+                CountdownCallback.Invoke(1 - (_countDownCounter / _secondsToWait));
+                yield return null;
+            }
+
         }
     }
+    [Serializable] public class CountdownCallback : UnityEvent<float> { }
 }
